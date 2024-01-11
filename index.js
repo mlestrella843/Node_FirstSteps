b53fec3d7f4bb18b3685fa87c33cadd91a1dea08
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-01-11 07:37:22 -0700

    CommonJs

---
+++ index.js
@@ -0,0 +1,3 @@
+//Using commonJs you have to use require.
+const sum = require('./sum');
+console.log(sum(1,2));
---
+++ sum.js
@@ -0,0 +1,5 @@
+//Use of commonJs
+function sum (a,b){
+    return a + b;
+}
+module.exports = sum;
