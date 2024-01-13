e2f9c25bb76503b830d7b0cfc30850629f5791c0
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-01-13 15:10:27 -0700

    working with ls

---
+++ 12.ls-advanced.js
@@ -0,0 +1,18 @@
+//* Usando Promises
+const fs = require('node:fs/promises');
+
+const folder = process.argv[2] ?? '.'; 
+
+fs.readdir(folder)
+.then(files => {
+        files.forEach(file => {
+            console.log(file);
+        })
+    })
+.catch(err => {
+        if(err) {
+            console.error('Error al leer el archivo', err)
+            return;
+        }
+    })
+
