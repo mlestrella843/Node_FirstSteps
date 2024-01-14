48ed16d651087f0b6d108dfbca5340c295606a66
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-01-13 20:06:53 -0700

    picocolors

--- 12.ls-advanced.js
+++ 12.ls-advanced.js
@@ -3,19 +3,20 @@ const { promises } = require('node:dns');
 const fs = require('node:fs/promises');
 const path = require('node:path');
 const folder = process.argv[2] ?? '.'; 
+const pc = require('picocolors');
 
 async function ls (folder){
     let files
         try {
             files = await fs.readdir(folder)     
         } catch{
-            console.error(`No se pudo leer el directorio ${folder}`)
+            console.error(pc.red(`No se pudo leer el directorio ${folder}`))
             process.exit(1)
         }
 
         const filesPromises = files.map(async file => {
             const filePath = path.join(folder,file)
-            let stats;
+            let stats
             
             try {
                 stats = await fs.stat(filePath)
