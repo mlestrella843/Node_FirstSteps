a4854dc52aeb929711fd35385bb9aa995fd73c69
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-01-14 10:02:19 -0700

    Native node modules finished

--- 13.http.js
+++ 13.http.js
@@ -1,9 +1,23 @@
 const http = require('node:http')
+const { findAvailablePort } = require('./14.free-port.js')
+
+const desirePort = process.env.PORT ?? 3000
 
 const server = http.createServer((req,res) => {
     console.log('request received')
     res.end('Hola Mundo')
 })
+
+
+//* Aqui podemos importar el modulo de findAvailablePort para que nos busque el puerto deseado.
+//* Podemos especificarle un puerto deseado y el nos dira si esta disponible o no.
+findAvailablePort(desirePort).then(port => {
+    server.listen(port, () => {
+        console.log(`Server listening port http://localhost:${port}`)
+    })
+})
+
+
 
 //* Cuando escogemos el puerto 0, le indicamos que escoge el primer puesto vacio que encuentre.
 //* En la vida real, para produccion esta practica no es recomendable, pues necesitas un puerto fijo
@@ -12,7 +26,7 @@ const server = http.createServer((req,res) => {
 //     console.log(`Server listen on port http://localhost:${server.address().port}`)
 // })
 
-//* Puerto fijo para el servidor
-server.listen(3002, () => {
-    console.log('Server listen on port 3002')
-})
+// //* Puerto fijo para el servidor
+// server.listen(3002, () => {
+//     console.log('Server listen on port 3002')
+// })
