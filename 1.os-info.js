878e41c5e95691421d1437ab5d917eec7165b3fc
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-01-14 08:14:34 -0700

    fixing

--- 1.os-info.js
+++ 1.os-info.js
@@ -1,12 +1,12 @@
-const os = require('node:os');
+const os = require('node:os')
 
-console.log('Info about own Operative System');
-console.log('--------------------------------');
+console.log('Info about own Operative System')
+console.log('--------------------------------')
 
-console.log('Operative System Name: ', os.platform());
-console.log('Operative System Version: ', os.version());
-console.log('Operative System Architecture: ', os.arch());
-console.log('Operative System CPUS: ', os.cpus());
-console.log('Operative System Free Memory: ', os.freemem() / 1024 / 1024);
-console.log('Operative System Total Memory: ', os.totalmem() / 1024 / 1024);
-console.log('Operative System uptime: ', os.uptime() / 60 / 60 / 24  + " days");
+console.log('Operative System Name: ', os.platform())
+console.log('Operative System Version: ', os.version())
+console.log('Operative System Architecture: ', os.arch())
+console.log('Operative System CPUS: ', os.cpus())
+console.log('Operative System Free Memory: ', os.freemem() / 1024 / 1024)
+console.log('Operative System Total Memory: ', os.totalmem() / 1024 / 1024)
+console.log('Operative System uptime: ', os.uptime() / 60 / 60 / 24 + ' days')
---
+++ 12.ls-advanced2.js
@@ -0,0 +1,40 @@
+//* Usando Promises
+const fs = require('node:fs/promises')
+const path = require('node:path')
+const folder = process.argv[2] ?? '.'
+const pc = require('picocolors')
+
+async function ls (folder){
+    let files
+        try {
+            files = await fs.readdir(folder)     
+        } catch{
+            console.error(pc.red(`No se pudo leer el directorio ${folder}`))
+            process.exit(1)
+        }
+
+        const filesPromises = files.map(async file => {
+        const filePath = path.join(folder,file)
+        let stats
+        
+        try {
+            stats = await fs.stat(filePath)
+        }catch{
+            console.error(`No se puedo leer el archivo ${filePath}`)
+            process.exit(1)
+        }
+
+        const isDirectory = stats.isDirectory()
+        const fileType = isDirectory ? 'd ' : 'f '
+        const fileSize = stats.size.toString()
+        const fileModified = stats.mtime.toLocaleString()
+
+        return `${pc.bgMagenta(fileType)}${pc.blue(file.padEnd(20))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`  
+        })
+            
+        const filesInfo = await Promise.all(filesPromises)
+
+        filesInfo.forEach(filesInfo => console.log(filesInfo))
+    }
+
+    ls(folder)
