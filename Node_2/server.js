be62f77e8fe39b74283f8062b1fed9366664faa9
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-01-28 10:40:19 -0700

    apis

--- Node_2/server.js
+++ Node_2/server.js
@@ -7,8 +7,8 @@ const server = http.createServer((req,res) => {
     res.end('Hola Mundo')
 })
 
-//* Aqui podemos importar el modulo de findAvailablePort para que nos busque el puerto deseado.
-//* Podemos especificarle un puerto deseado y el nos dira si esta disponible o no.
+// Aqui podemos importar el modulo de findAvailablePort para que nos busque el puerto deseado.
+// Podemos especificarle un puerto deseado y el nos dira si esta disponible o no.
     server.listen(desiredPort, () => {
         console.log(`Server listening port http://localhost:${desiredPort}`)
     })
--- Node_2/server1.js
+++ Node_2/server1.js
@@ -1,12 +1,22 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req,res) => {
     res.setHeader('content-Type', 'text/html; charset=utf-8')
     if(req.url === '/'){
-        res.statusCode = 200 // es ok
-        res.end('<h1>Welcome to my Home Page, Página</h1>')
+        res.end('<h1>Welcome Mi Página</h1>')
+    }else if(req.url === '/imagen-maria-super-linda.png'){
+        fs.readFile('./maria.png', (err, data) => {
+            if(err){
+                res.statusCode=500
+                res.end('<h1>500 Internal Server Error</h1>')
+            }else{
+                res.setHeader('Content-Type', 'image/png')
+                res.end(data)
+            }
+        })
     }else if(req.url === '/contacto'){
         res.statusCode = 200 // es ok
         res.end('<h1>Contact</h1>')
@@ -18,8 +28,8 @@ const processRequest = (req,res) => {
 
 const server = http.createServer(processRequest)
 
-//* Aqui podemos importar el modulo de findAvailablePort para que nos busque el puerto deseado.
-//* Podemos especificarle un puerto deseado y el nos dira si esta disponible o no.
+// Aqui podemos importar el modulo de findAvailablePort para que nos busque el puerto deseado.
+// Podemos especificarle un puerto deseado y el nos dira si esta disponible o no.
     server.listen(desiredPort, () => {
         console.log(`Server listening port http://localhost:${desiredPort}`)
     })
