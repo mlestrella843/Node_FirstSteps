8056427f6544d72e662281849f286cd5af7dbc81
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-04 09:12:36 -0700

    Creating methods

--- Node_2/routing.js
+++ Node_2/routing.js
@@ -16,6 +16,37 @@ const processRequest = (req,res) => {
                 res.setHeader('Content-Type', 'text/html;charset=utf-8')
                 return res.end('<h1>Error 404</h1>')
             }
+
+        case 'POST':
+            switch (url) {
+                case '/pokemon': {
+                    let body = ''
+                 
+                    req.on('data', chunk => {
+                        body+= chunk.toString()
+                    })
+
+                    req.on('end', () => {
+                        const data = JSON.parse(body)
+                        res.writeHead(201, { 
+                            'Content-type': 'application/json; charset=utf-8'
+                        })
+                        data.timestamp = Date.now()
+                        res.end(JSON.stringify(data))
+                    })
+
+                    break
+
+
+                }  
+            default: 
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'text/plain;charset=utf-8')
+            return res.end('404 Not Found')   
+        
+                
+                
+            }
     }
 }
 
