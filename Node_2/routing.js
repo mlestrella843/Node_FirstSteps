7e2ca879b2152356744e7c61302482b47d76cf25
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-04 08:49:22 -0700

    Structuring apis

--- Node_2/routing.js
+++ Node_2/routing.js
@@ -1,7 +1,22 @@
 const http = require('node:http')
 
-const processRequest = (req,res) => {
+const dittoJSON = require('./pokemon/ditto.json')
 
+const processRequest = (req,res) => {
+    const { method, url } = req
+
+    switch (method) {
+        case 'GET':
+            switch (url) {
+                case '/pokemon/ditto':
+                    res.setHeader('Content-Type', 'application/json;charset=utf-8')
+                    return res.end(JSON.stringify(dittoJSON))
+            default: 
+                res.statusCode = 404
+                res.setHeader('Content-Type', 'text/html;charset=utf-8')
+                return res.end('<h1>Error 404</h1>')
+            }
+    }
 }
 
 const server = http.createServer(processRequest)
