da317e175914e7453f960df81dd8a20a2ce23980
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2024-02-06 02:42:03 -0700

    Testing endpoints

--- Node_3/app.js
+++ Node_3/app.js
@@ -13,6 +13,13 @@ app.get('/movies', (req,res) => {
     res.json(movies);
 });
 
+app.get('/movies/:id', (req,res) => { //path to regexp
+    const { id } = req.params;
+    const movie = movies.find(movie => movie.id === id);
+    if (movie) return res.json(movie);
+    res.status(404).json({ message: 'Movie not found' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
